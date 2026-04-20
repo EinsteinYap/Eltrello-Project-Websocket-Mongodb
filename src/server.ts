0b88ebe6cee00d53f2e9c125ed7f15a3bdf09139
EinsteinYap <einsteinyap2014@gmail.com>
2026-04-20 12:38:29 +0800

    auth api done

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,24 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import mongoose from "mongoose";
+import * as usersController from "./controllers/users";
+import bodyParser from "body-parser";
+import authMiddleware from './middlewares/auth'
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("API is UP");
 });
+
+app.post("/api/users", usersController.register);
+app.post("/api/users/login", usersController.login);
+app.get('/api/user', authMiddleware, usersController.currentUser)
 
 io.on("connection", () => {
   console.log("connect");
@@ -18,6 +28,6 @@ io.on("connection", () => {
 mongoose.connect("mongodb://localhost:27017/eltrello").then(() => {
   console.log("connected to mongodb");
   httpServer.listen(4001, () => {
-    console.log(`API is listening on port http://localhost:4001`);
+    console.log(`API is listening on port 4001`);
   });
 });
